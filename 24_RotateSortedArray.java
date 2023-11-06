8d70cff54372d9f43a78c992a0b7d30e604e8c42
Atharva Jamdar <76987608+Atharva1479@users.noreply.github.com>
2023-11-06 11:32:14 +0530

    Add files via upload

---
+++ 24_RotateSortedArray.java
@@ -0,0 +1,51 @@
+/* 189. Rotate Array - Medium
+
+Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+Example 1:
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+
+Example 2:
+Input: nums = [-1,-100,3,99], k = 2
+Output: [3,99,-1,-100]
+Explanation: 
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]
+ 
+Constraints:
+1 <= nums.length <= 105
+-231 <= nums[i] <= 231 - 1
+0 <= k <= 105 */
+
+class Solution {
+
+    public void rotate(int[] nums, int k) {
+
+        k = k % nums.length;
+        int n = nums.length;
+        
+        reverseArray(nums, 0, n-1);
+        reverseArray(nums, 0, k-1);
+        reverseArray(nums, k, n-1);
+    }
+
+    public static void reverseArray(int[] nums, int start, int end) {
+
+        while(start <= end) {
+
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+
+            start++;
+            end--;
+        }
+    }
+}
+//Time: O(n)
+//Space: O(1)
