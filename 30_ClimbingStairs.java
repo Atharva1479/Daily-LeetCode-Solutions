431c54b21f1ab9956a62464d3a246e61dd928812
Atharva Jamdar <76987608+Atharva1479@users.noreply.github.com>
2023-11-12 10:49:48 +0530

    Add files via upload

---
+++ 30_ClimbingStairs.java
@@ -0,0 +1,64 @@
+/* 70. Climbing Stairs - Easy
+
+You are climbing a staircase. It takes n steps to reach the top.
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+Example 1:
+Input: n = 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+
+Example 2:
+Input: n = 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+ 
+Constraints:
+1 <= n <= 45 */
+
+
+/* Approach 1: Recursion:
+
+class Solution {
+
+    public int climbStairs(int n) {
+
+        if (n == 0 || n == 1) {
+
+            return 1;
+        }
+        return climbStairs(n-1) + climbStairs(n-2);
+    }
+} 
+Time Complexity: O(2^n)
+Space Complexity: O(n) 
+*/
+
+//Approach 2: Space Optimization:
+
+class Solution {
+
+    public int climbStairs(int n) {
+
+        if (n == 0 || n == 1) {
+
+            return 1;
+        }
+
+        int prev = 1, curr = 1;
+        for (int i = 2; i <= n; i++) {
+
+            int temp = curr;
+            curr = prev + curr;
+            prev = temp;
+        }
+        return curr;
+    }
+}
+//Time Complexity: O(n)
+//Space Complexity: O(1)
