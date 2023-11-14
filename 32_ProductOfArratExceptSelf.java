29adbdd858eb6df2734b678cc6bc482f37301214
Atharva Jamdar <76987608+Atharva1479@users.noreply.github.com>
2023-11-14 11:18:41 +0530

    Add files via upload

---
+++ 32_ProductOfArratExceptSelf.java
@@ -0,0 +1,49 @@
+/* 238. Product of Array Except Self - Medium
+
+Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+You must write an algorithm that runs in O(n) time and without using the division operation.
+
+Example 1:
+Input: nums = [1,2,3,4]
+Output: [24,12,8,6]
+
+Example 2:
+Input: nums = [-1,1,0,-3,3]
+Output: [0,0,9,0,0]
+ 
+Constraints:
+2 <= nums.length <= 105
+-30 <= nums[i] <= 30
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer. */
+
+class Solution {
+
+    public int[] productExceptSelf(int[] nums) {
+
+        int n = nums.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        int[] answer = new int[n];
+
+        left[0] = 1;
+        for (int i = 1; i < n; i++) {
+            
+            left[i] = left[i - 1] * nums[i - 1];
+        }
+
+        right[n - 1] = 1;
+        for (int i = n - 2; i >= 0; i--) {
+
+            right[i] = right[i + 1] * nums[i + 1];
+        }
+
+        for (int i = 0; i < n; i++) {
+
+            answer[i] = left[i] * right[i];
+        }
+        return answer;
+    }
+}
+//Time complexity: O(n) 
+//Space complexity: O(n)
