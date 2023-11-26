eb897c7d49cfac482992b6026b1bb081b484fc23
Atharva Jamdar <76987608+Atharva1479@users.noreply.github.com>
2023-11-26 11:04:57 +0530

    Add files via upload

---
+++ 44_Largest Submatrix With Rearrangements.java
@@ -0,0 +1,61 @@
+/* 1727. Largest Submatrix With Rearrangements - Medium
+
+You are given a binary matrix matrix of size m x n, and you are allowed to rearrange the columns of the matrix in any order.
+Return the area of the largest submatrix within matrix where every element of the submatrix is 1 after reordering the columns optimally.
+
+Example 1:
+Input: matrix = [[0,0,1],[1,1,1],[1,0,1]]
+Output: 4
+Explanation: You can rearrange the columns as shown above.
+The largest submatrix of 1s, in bold, has an area of 4.
+
+Example 2:
+Input: matrix = [[1,0,1,0,1]]
+Output: 3
+Explanation: You can rearrange the columns as shown above.
+The largest submatrix of 1s, in bold, has an area of 3.
+
+Example 3:
+Input: matrix = [[1,1,0],[1,0,1]]
+Output: 2
+Explanation: Notice that you must rearrange entire columns, and there is no way to make a submatrix of 1s larger than an area of 2.
+ 
+Constraints:
+m == matrix.length
+n == matrix[i].length
+1 <= m * n <= 105
+matrix[i][j] is either 0 or 1. */
+
+class Solution {
+
+    public int largestSubmatrix(int matrix[][]) {
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        for (int i = 1; i < m; ++i) {
+
+            for (int j = 0; j < n; ++j) {
+
+                if (matrix[i][j] == 1) {
+
+                    matrix[i][j] = matrix[i - 1][j] + 1;
+                }
+            }
+        }
+
+        int ans = 0;
+        for (var row : matrix) {
+
+            Arrays.sort(row);
+            for (int j = n - 1, k = 1; j >= 0 && row[j] > 0; --j, ++k) {
+
+                int s = row[j] * k;
+                ans = Math.max(ans, s);
+            }
+        }
+        return ans;
+    }
+}
+//Time Complexity: O(m * n * log(n)) - Sorting is done for each row.
+//Space Complexity: O(1) - No additional space is used except for variables.
