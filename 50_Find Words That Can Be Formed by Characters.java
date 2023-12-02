846a6ae0f32f1c49f4d6e80e76f63143d5593405
Atharva Jamdar <76987608+Atharva1479@users.noreply.github.com>
2023-12-02 10:58:17 +0530

    Add files via upload

---
+++ 50_Find Words That Can Be Formed by Characters.java
@@ -0,0 +1,60 @@
+/* 1160. Find Words That Can Be Formed by Characters - Easy
+
+You are given an array of strings words and a string chars.
+A string is good if it can be formed by characters from chars (each character can only be used once).
+Return the sum of lengths of all good strings in words.
+
+Example 1:
+
+Input: words = ["cat","bt","hat","tree"], chars = "atach"
+Output: 6
+Explanation: The strings that can be formed are "cat" and "hat" so the answer is 3 + 3 = 6.
+
+Example 2:
+Input: words = ["hello","world","leetcode"], chars = "welldonehoneyr"
+Output: 10
+Explanation: The strings that can be formed are "hello" and "world" so the answer is 5 + 5 = 10.
+ 
+Constraints:
+1 <= words.length <= 1000
+1 <= words[i].length, chars.length <= 100
+words[i] and chars consist of lowercase English letters. */
+
+class Solution {
+
+    public int countCharacters(String[] words, String chars) {
+
+        int[] counts = new int[26];
+        
+        for(int i=0;i<chars.length();i++) {
+
+            counts[chars.charAt(i)-'a']++;
+        }
+
+        int res =0 ;
+        
+        for(String s : words) {
+
+            if(canForm(s,counts))
+            
+            res+=s.length();
+        }
+        return res;
+    }
+
+    boolean canForm(String word, int[] counts) {
+        
+        int[] c = new int[26];
+
+        for(int i =0;i<word.length();i++) {
+
+            int x = word.charAt(i)-'a';
+            c[x]++;
+            if(c[x] > counts[x])
+                return false;
+        }
+        return true;
+    }
+}
+//Time complexity: O(m + n), where m is the length of the chars string, and n is the length of the words array.
+//Space complexity: O(1) 
