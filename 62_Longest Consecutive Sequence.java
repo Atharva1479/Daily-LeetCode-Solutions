a06871fb049d12bd49b30b576fa98c101e1d26a2
Atharva Jamdar <76987608+Atharva1479@users.noreply.github.com>
2023-12-17 10:43:12 +0530

    Add files via upload

---
+++ 62_Longest Consecutive Sequence.java
@@ -0,0 +1,53 @@
+/*
+128. Longest Consecutive Sequence - Medium
+
+Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+
+You must write an algorithm that runs in O(n) time.
+
+Example 1:
+Input: nums = [100,4,200,1,3,2]
+Output: 4
+Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+
+Example 2:
+Input: nums = [0,3,7,2,5,8,4,6,0,1]
+Output: 9
+
+Constraints:
+0 <= nums.length <= 105
+-109 <= nums[i] <= 109
+*/
+
+class Solution {
+
+    public int longestConsecutive(int[] nums) {
+
+        int n = nums.length;
+        if(n<2) return n;
+
+        int result = 1, count = 1;
+        Arrays.sort(nums);
+
+        for(int i=1; i<n; i++) {
+
+            if(i<n && nums[i] == nums[i-1] + 1) {
+
+                count++;
+            } 
+            else if(i<n && nums[i] == nums[i-1]) {
+
+                continue;
+            } 
+            else {
+                
+                result = Math.max(result, count);
+                count = 1;
+            }
+        }
+        result = Math.max(result, count);
+        return result;
+    }
+}
+//Time complexity: O(nlogn)
+//Space complexity: O(1)
